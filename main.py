430f4b0c542f82e64e0f065edbd836e65345bccf
AliMarashi <89460284+AliMarashi@users.noreply.github.com>
2022-09-13 19:32:52 +0430

    init

--- main.py
+++ main.py
@@ -2,14 +2,34 @@
 
     """
 
+import json
+from pathlib import Path
+
 import pandas as pd
+from githubdata import GithubData
 
+class GDUrl:
+    with open('gdu.json' , 'r') as f:
+        gj = json.load(f)
 
+    src = gj['src']
+
+gu = GDUrl()
 
 def main():
     pass
 
     ##
+    gds = GithubData(gu.src)
+    ##
+    gds.overwriting_clone()
+    ##
+    fps = list(gds.local_path.glob('*.xlsx'))
+    ##
+    df = pd.DataFrame()
+    df['xlp'] = fps
+
+
 
     ##
 
