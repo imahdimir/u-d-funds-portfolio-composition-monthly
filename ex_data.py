3c1ed93d0d04f8937452c4fc457ea0cb03483f95
AliMarashi <89460284+AliMarashi@users.noreply.github.com>
2022-09-18 18:03:32 +0430

    updt

--- ex_data.py
+++ ex_data.py
@@ -20,7 +20,7 @@ gu = GDUrl()
 
 
 class Params :
-    min_pct_ch = .7
+    min_pct_ch = .9
     min_pct_num = .9
     min_pct_nan_col = .3
     min_pct_nan_row = .7
@@ -30,15 +30,18 @@ p = Params()
 
 
 def clean_row_character(df) :
-    df1 = df.fillna(0)
-    df1 = df1.reset_index(drop = True)
+    df = df.fillna(0)
+    df = df.reset_index(drop = True)
 
-    for ind , _ in df1.iterrows() :
-        df1.loc[ind , :] = df1.loc[ind , :].astype('string')
-        pct_num = df1.loc[ind , :].str.isalpha().sum() / len(df1.columns)
+    for ind , _ in df.iterrows() :
+        df.loc[ind , :] = df.loc[ind , :].astype('string')
+        pct_num = df.loc[ind , :].str.isalpha().sum() / len(df.columns)
         if pct_num >= p.min_pct_ch :
             df = df.drop(index = ind)
 
+    df = df.fillna(0)
+    df = df[df.iloc[: , 0] != 0]
+    df = df[~df.iloc[: , 0].str.contains('نام صندوق')]
     return df
 
 
@@ -56,15 +59,35 @@ def clean_column_character(df) :
     return df
 
 
-def clean_column_numeric(df) :
+def clean_column_numeric(df , column_num) :
     df1 = df.fillna(0)
 
     for i , cn in enumerate(df1.columns) :
-        if i <= 2 :
+        df1[cn] = df1[cn].astype('string')
+        pct_chr = df1[cn].str.fullmatch(r'\D+').sum() / len(df1)
+        if i <= column_num :
             df1[cn] = df1[cn].astype('string')
             pct_num = df1[cn].str.fullmatch(r'\d*\.?\d+').sum() / len(df1)
+            if pct_chr >= 0.5 :
+                break
             if pct_num >= p.min_pct_num :
                 df = df.drop(columns = cn)
+
+    return df
+
+
+def clean_column_High_value(df) :
+    df1 = df.fillna(0)
+    df1 = df1.iloc[: , 1 :]
+    df1 = df1.replace('_' , 0)
+    df1 = df1.replace('-' , 0)
+    df1 = df1.astype('float')
+    df1 = df1.select_dtypes(exclude = ['object'])
+
+    for cn in df1.columns :
+
+        if df1.loc[: , cn].mean() >= 100 :
+            df = df.drop(columns = cn)
 
     return df
 
@@ -93,26 +116,46 @@ def main() :
     ##
     fps = list(gds.local_path.glob('*.xlsx'))
     ##
-    df_list0 = []
-    for i in tqdm(range(1 , len(fps))) :
+    df_list0_0 = []
+    for i in tqdm(range(1 , 119)) :
         df = pd.read_excel(fps[i] , sheet_name = 1 , header = 0)
         df = clean_row_nan(df)
         df = clean_column_nan(df)
         # df.iloc[:,0] = df.iloc[:,0].astype(int)
-        df = clean_column_numeric(df)
+        df = clean_column_numeric(df , 2)
         df = clean_row_character(df)
         df = clean_column_character(df)
+        # df = clean_column_High_value(df)
         df = df.iloc[: , [0 , 1 , 2 , 3 , 4 , 5 , 6]]
         df.columns = range(len(df.columns))
         df['Date'] = fps[i].stem
-        df_list0.append(df)
+        df_list0_0.append(df)
 
-    df0 = pd.concat(df_list0 , axis = 0 , ignore_index = False)
+    df0_0 = pd.concat(df_list0_0 , axis = 0 , ignore_index = False)
+    df_list0_1 = []
+    for i in tqdm(range(119 , len(fps))) :
+        df = pd.read_excel(fps[i] , sheet_name = 1 , header = 0)
+        df = clean_row_nan(df)
+        df = clean_column_nan(df)
+        # df.iloc[:,0] = df.iloc[:,0].astype(int)
+        df = clean_column_numeric(df , 2)
+        df = clean_row_character(df)
+        df = clean_column_character(df)
+        # df = clean_column_High_value(df)
+        df = df.iloc[: , [0 , 2 , 3 , 4 , 5 , 6 , 7]]
+        df.columns = range(len(df.columns))
+        df['Date'] = fps[i].stem
+        df_list0_1.append(df)
+
+    df0_1 = pd.concat(df_list0_1 , axis = 0 , ignore_index = False)
+
+    df0 = pd.concat([df0_0 , df0_1] , axis = 0 , ignore_index = False)
     df1 = df0.copy()
     ##
     df1 = df1.fillna(0)
     df1 = df1[df1.iloc[: , 0] != 0]
-    df1 = df1[~df1[0].str.contains('نام صندوق')]
+    df1 = df1[df1.iloc[: , 0] != "0"]
+    # df1 = df1[~df1[0].str.contains('نام صندوق')]
     df1 = df1.reset_index(drop = True)
 
     df1 = df1.replace('_' , 0)
@@ -122,6 +165,7 @@ def main() :
     ##
     df1.columns = [str(x) for x in df1.columns]
     df1['0'] = df1['0'].astype('string')
+    df1['6'] = df1['6'].astype('string')
     df1.to_parquet('t.prq' , index = False)
 
     ##
--- match.py
+++ match.py
@@ -20,20 +20,45 @@ def main() :
 
     ##
     df1 = pd.read_parquet('t.prq')
-    df1.columns = ['Name' , 'Share' , 'Bond' , 'Deposit' , 'Cash' ,
+    df1.columns = ['Name' , 'Value' , 'Share' , 'Bond' , 'Deposit' , 'Cash' ,
                    'Other' , 'JMonths']
     ##
     Final = df.merge(df1 , on = ["Name"])
-    Final = Final.sort_values(by = ["InstituteKind" , 'Name' , "JMonths"])
+    Final = Final.sort_values(by = ["JMonths"])
+
+    # Final = Final.sort_values(by = ["InstituteKind" , 'Name' , "JMonths"])
     Final = Final[
-        ['JMonths' , 'SEORegisterNo', 'Share' , 'Bond' , 'Deposit' ,
-         'Cash' , 'Other']]
-    # Final.JMonths = Final.JMonths.str.replace('-' , '').astype(int)
-    # Final = Final.astype(float)
-    # Final.SEORegisterNo = Final.SEORegisterNo.astype('string')
+        ['JMonths' , 'SEORegisterNo' , 'InstituteKind' , 'Value' , 'Share' ,
+         'Bond' , 'Deposit' , 'Cash' , 'Other']]
+
+    Final.SEORegisterNo = Final.SEORegisterNo.astype('string')
+    Final[['Share' , 'Bond' , 'Deposit' , 'Cash' , 'Other']] = Final[
+        ['Share' , 'Bond' , 'Deposit' , 'Cash' , 'Other']].astype('float')
     Final['Sum'] = Final['Share'] + Final['Bond'] + Final['Deposit'] + Final[
         'Cash'] + Final['Other']
-    Final.dtypes
+    Final = Final[Final["Sum"] <= 110]
+    Final = Final.reset_index(drop = True)
+
+    Final.columns
+    ##
+    Final.InstituteKind.unique()
+    ##
+    Result = Final[Final["InstituteKind"] == "در اوراق بهادار با درآمد ثابت"]
+    Result = Result.groupby(["JMonths"]).mean()
+    Result = Result.reset_index()
+    Result = Result[Result['Share'] <= 100]
+    ##
+    import pandas_bokeh
+
+    Result.plot(kind = 'line' ,
+                x = 'JMonths' ,
+                y = ['Share' , 'Bond' , 'Deposit' , 'Cash' , 'Other'])
+    # Result.plot_bokeh.line(x = ['JMonths'] ,
+    #                        y = ['Share' , 'Bond' , 'Deposit' , 'Cash' ,
+    #                             'Other'] ,
+    #                        figsize = (900 , 500))
+
+    plt.show()
 
     ##
 
